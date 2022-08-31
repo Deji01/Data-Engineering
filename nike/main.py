84b6a3e468042bfdd22cce9c27b1f724f4567100
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-31 04:35:20 +0000

    bug fixed

--- nike/main.py
+++ nike/main.py
@@ -28,13 +28,13 @@ headers = {
 
 count = 60
 step = 1
-# 60, 1440, 60
-for anchor in range(60, 120, 60):
+
+for anchor in range(60, 1500, 60):
     url = f"https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=4BDA24CABADC363265C54C3502599558&country=us&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(US)%26filter%3Dlanguage(en)%26filter%3DemployeePrice(true)%26searchTerms%3Dsneakers%26anchor%3D{anchor}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D{count}&language=en&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D"
 
     print(f'Start : Anchor {anchor}')
     response = requests.get(url, headers=headers)
-    result = response.text
+    result = response.json()
     print(type(result))
   
     filename = f"{datetime.now().strftime('%d-%m-%Y')}-anchor-{anchor}.json"
