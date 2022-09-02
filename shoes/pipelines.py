3bf3f3398f81e07f05db581963e94b16fa6267ea
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-09-02 21:40:45 +0000

    fix connection

--- shoes/pipelines.py
+++ shoes/pipelines.py
@@ -5,7 +5,7 @@
 
 
 # useful for handling different item types with a single interface
-from ..config import db_host, db_name, db_password, db_port, db_user
+from shoes.config import db_host, db_name, db_password, db_port, db_user
 from itemadapter import ItemAdapter
 import psycopg2
 import sys
@@ -24,14 +24,20 @@ class SaveToPostgreSQLPipeline(object):
     def create_connection(self):
         "Create Database Connection"
 
+        host = db_host
+        dbname = db_name
+        user = db_user
+        password = db_password
+        sslmode = "require"
+
+        # Constructing connection string
+        conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(
+            host, user, dbname, password, sslmode
+        )
+
         try:
-            self.connection = psycopg2.connect(
-                host=db_host,
-                user=db_user,
-                password=db_password,
-                database=db_name,
-                port=db_port,
-            )
+            self.connection = psycopg2.connect(conn_string)
+            print("Connection established")
 
         except psycopg2.Error as e:
             print(f"Error connecting to Postgres DB : {e}")
