00f06633299a590e464088341e9b1343971e471a
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-12-01 22:05:36 +0000

    update config

--- shoes/config.py
+++ shoes/config.py
@@ -1,9 +1,7 @@
-from kaggle_secrets import UserSecretsClient
+import os
 
-user_secrets = UserSecretsClient()
-
-db_host = user_secrets.get_secret("DB_HOST")
-db_name = user_secrets.get_secret("DB_NAME")
-db_password = user_secrets.get_secret("DB_PASSWORD")
-db_port = user_secrets.get_secret("DB_PORT")
-db_user = user_secrets.get_secret("DB_USER")
+db_host = os.environ["DB_HOST"]
+db_name = os.environ["DB_NAME"]
+db_password = os.environ["DB_PASSWORD"]
+db_port = os.environ["DB_PORT"]
+db_user = os.environ["DB_USER"]
